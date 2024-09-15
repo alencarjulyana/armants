5e0f01787450fb4d4b6567338797e857b85db736
alencarjulyana <julyanasalencar@gmail.com>
2024-09-15 20:32:41 -0300

    Tela de login faltando aucorreção das autenticações

---
+++ src/main/java/br/edu/ifpb/pweb2/armants/controller/AuthController.java
@@ -0,0 +1,16 @@
+package br.edu.ifpb.pweb2.armants.controller;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
+
+
+@Controller
+@RequestMapping("/auth")
+public class AuthController {
+
+    @GetMapping("/login")
+    public ModelAndView getForm(ModelAndView mav) {
+        mav.setViewName("login/login");
+        return mav;
+    }
+}
